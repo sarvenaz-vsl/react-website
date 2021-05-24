7f5691fb0410c403b86b56545d47ec8490e0acb9
sarvenaz-vsl <sarvenaz.vsl@gmail.com>
2021-05-24 19:08:31 +0430

    Products and Services pages developed

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import Navbar from './components/Navbar';
 import Footer from './components/pages/Footer/Footer';
 import Home from './components/pages/HomePage/Home';
+import Products from './components/pages/Products/Products';
+import Services from './components/pages/Services/Services';
 
 function App() {
   return (
@@ -10,6 +12,8 @@ function App() {
       <Navbar />
       <Switch>
         <Route path='/' exact component={Home} />
+        <Route path='/services' component={Services} />
+        <Route path='/products' component={Products} />
       </Switch>
       <Footer />
     </Router>
